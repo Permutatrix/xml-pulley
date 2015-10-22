43620d9f288652730340d51be04ab783db857703
Permutator <permutatorem@gmail.com>
2015-10-22 16:20:28 -0700

    Improved implementation

--- src/xml-pulley.js
+++ src/xml-pulley.js
@@ -1,5 +1,4 @@
 import { parser as saxParser } from 'sax';
-import Queue from './queue.js';
 
 function isAllowedType(type) {
   switch(type) {
@@ -11,161 +10,171 @@ function isAllowedType(type) {
   }
 }
 
-class XMLPulley {
-  constructor(xml, options) {
-    if(!options) options = {};
-    let types = options.types || ['opentag', 'closetag', 'text'];
-    let queue = this.queue = new Queue();
-    let parser = saxParser(true, {
-      xmlns: options.xmlns,
-      position: false
-    });
-    let skipWS = options.skipWhitespaceOnly;
-    let trim = options.trim, normalize = options.normalize;
-    function textOpts(t) {
-      if(trim)
-        t = t.trim();
-      if(normalize)
-        t = t.replace(/\s+/g, " ")
-      return t;
+export function makePulley(xml, options) {
+  let self;
+  
+  if(!options) options = {};
+  let types = options.types || ['opentag', 'closetag', 'text'];
+  let queue = [];
+  let parser = saxParser(true, {
+    xmlns: options.xmlns,
+    position: false
+  });
+  let skipWS = options.skipWhitespaceOnly;
+  let trim = options.trim, normalize = options.normalize;
+  let textOpts = trim || normalize ? (t) => {
+    if(trim)
+      t = t.trim();
+    if(normalize)
+      t = t.replace(/\s+/g, " ")
+    return t;
+  } : (t) => t;
+  let text = null, rawText = null;
+  let flushText = () => {
+    if(text !== null) {
+      queue.push({
+        type: 'text',
+        text: text,
+        rawText: rawText
+      });
+      text = rawText = null;
     }
-    let text = null, rawText = null;
-    let flushText = () => {
-      if(text !== null) {
-        this.queue.enqueue({
-          type: 'text',
-          text: text,
-          rawText: rawText
-        });
-        text = rawText = null;
-      }
-    };
-    parser.onerror = (err) => {
-      throw err;
-    };
-    types.forEach((type) => {
-      if(type === 'text') {
-        parser.ontext = (t) => {
-          if(!skipWS || /\S/.test(t)) {
-            let pt = textOpts(t);
-            if(text) {
-              text += pt; rawText += t;
-            } else {
-              text = pt; rawText = t;
-            }
-          }
-        };
-        parser.oncdata = (t) => {
+  };
+  parser.onerror = (err) => {
+    throw err;
+  };
+  types.forEach((type) => {
+    if(type === 'text') {
+      parser.ontext = (t) => {
+        if(!skipWS || /\S/.test(t)) {
+          let pt = textOpts(t);
           if(text) {
-            text += t; rawText += t;
+            text += pt; rawText += t;
           } else {
-            text = rawText = t;
+            text = pt; rawText = t;
           }
-        };
-      } else if(type === 'comment') {
-        parser.oncomment = (t) => {
-          flushText();
-          queue.enqueue({
-            type: 'comment',
-            text: textOpts(t),
-            rawText: t
-          });
         }
-      } else if(type === 'doctype') {
-        parser.ondoctype = (t) => {
-          flushText();
-          queue.enqueue({
-            type: 'doctype',
-            text: t
-          });
+      };
+      parser.oncdata = (t) => {
+        if(text) {
+          text += t; rawText += t;
+        } else {
+          text = rawText = t;
         }
-      } else if(type === 'closetag') {
-        parser.onclosetag = (t) => {
-          flushText();
-          queue.enqueue({
-            type: 'closetag',
-            name: t
-          });
-        }
-      } else if(type === 'opencdata' || type === 'closecdata' ||
-                type === 'opennamespace' || type === 'closenamespace') {
-        parser['on'+type] = (data) => {
-          flushText();
-          queue.enqueue({ type: type });
-        }
-      } else if(isAllowedType(type)) {
-        parser['on'+type] = (data) => {
-          flushText();
-          data.type = type;
-          queue.enqueue(data);
-        }
-      } else {
-        throw new Error(`${type} isn't an allowed type!`);
+      };
+    } else if(type === 'comment') {
+      parser.oncomment = (t) => {
+        flushText();
+        queue.push({
+          type: 'comment',
+          text: textOpts(t),
+          rawText: t
+        });
       }
-    });
-    parser.write(xml).close();
-    flushText();
+    } else if(type === 'doctype') {
+      parser.ondoctype = (t) => {
+        flushText();
+        queue.push({
+          type: 'doctype',
+          text: t
+        });
+      }
+    } else if(type === 'closetag') {
+      parser.onclosetag = (t) => {
+        flushText();
+        queue.push({
+          type: 'closetag',
+          name: t
+        });
+      }
+    } else if(type === 'opencdata' || type === 'closecdata' ||
+              type === 'opennamespace' || type === 'closenamespace') {
+      parser['on'+type] = (data) => {
+        flushText();
+        queue.push({ type: type });
+      }
+    } else if(isAllowedType(type)) {
+      parser['on'+type] = (data) => {
+        flushText();
+        data.type = type;
+        queue.push(data);
+      }
+    } else {
+      throw new Error(`${type} isn't an allowed type!`);
+    }
+  });
+  parser.write(xml).close();
+  flushText();
+  queue.reverse();
+  
+  let next = () => {
+    return queue.pop();
   }
-  next() {
-    return this.queue.dequeue();
+  let peek = () => {
+    return queue[queue.length - 1];
   }
-  peek() {
-    return this.queue.peek();
-  }
-  check(type, error) {
-    let out = this.peek();
+  let check = (type, error) => {
+    let out = peek();
     assertType(out, type, error);
     return out;
   }
-  checkName(name, type, wrongNameError, wrongTypeError) {
+  let checkName = (name, type, wrongNameError, wrongTypeError) => {
     type = type || 'opentag';
-    let out = this.peek();
+    let out = peek();
     assertType(out, type, wrongTypeError || wrongNameError);
     assertName(out, name, wrongNameError);
     return out;
   }
-  expect(type, error) {
-    let out = this.check(type, error);
-    this.next();
+  let expect = (type, error) => {
+    let out = check(type, error);
+    next();
     return out;
   }
-  expectName(name, type, wrongNameError, wrongTypeError) {
-    let out = this.checkName(name, type, wrongNameError, wrongTypeError);
-    this.next();
+  let expectName = (name, type, wrongNameError, wrongTypeError) => {
+    let out = checkName(name, type, wrongNameError, wrongTypeError);
+    next();
     return out;
   }
-  loop(callback, endType) {
+  let loop = (callback, endType) => {
     endType = endType || 'closetag';
     let node;
-    while((node = this.peek()) && node.type !== endType) {
-      if(callback(this))
+    while((node = peek()) && node.type !== endType) {
+      if(callback(self))
         break;
     }
   }
-  loopTag(callback, name) {
-    let tag = name ? this.expectName(name) : this.expect('opentag');
+  let loopTag = (callback, name) => {
+    let tag = name ? expectName(name) : expect('opentag');
     let node;
-    while((node = this.peek()) && node.type !== 'closetag') {
-      callback(this, tag);
+    while((node = peek()) && node.type !== 'closetag') {
+      callback(self, tag);
     }
-    this.expectName(tag.name, 'closetag');
+    expectName(tag.name, 'closetag');
     return tag;
   }
-  skipTag(name) {
-    return this.loopTag((pulley) => {
+  let skipTag = (name) => {
+    return loopTag((pulley) => {
       let tag = pulley.peek();
       if(tag.type === 'opentag') {
-        this.skipTag(tag.name);
+        pulley.skipTag(tag.name);
       } else {
-        this.next();
+        pulley.next();
       }
     }, name);
   }
+  return self = {
+    next,
+    peek,
+    check,
+    checkName,
+    expect,
+    expectName,
+    loop,
+    loopTag,
+    skipTag
+  };
 }
 
-export function makePulley(xml, options) {
-  return new XMLPulley(xml, options);
-}
 
 export function assertType(node, type, error) {
   if(node === undefined) {
