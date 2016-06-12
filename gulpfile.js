33eef2472f3e4baf5a61fff94e18cf4ef73f5b2a
Permutator <permutatorem@gmail.com>
2016-06-11 23:24:04 -0700

    Sourcemap generation better?!?

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
   return gulp.src('src/**/*.js')
-  .pipe(babel({loose: 'all'}))
+  .pipe(babel({presets: ['es2015-loose']}))
   .pipe(gulp.dest('lib'));
 });
 
 gulp.task('with-sourcemap', function() {
   return gulp.src('src/**/*.js')
-  .pipe(babel({loose: 'all', sourceMaps: 'inline'}))
+  .pipe(sourcemaps.init())
+    .pipe(babel({presets: ['es2015-loose']}))
+  .pipe(sourcemaps.write({ sourceRoot: '../src' }))
   .pipe(gulp.dest('lib'));
 });
